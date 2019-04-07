b8b26f54482d26a02e82476f179325867bf0bd2b
Gabriel Costa Silva <gabriel@Gabriels-iMac.local>
2019-04-07 16:14:55 -0300

    - Wiped old structure;

--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/domain/base/MacroRegionRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.utfpr.cp.emater.midmipsystem.domain.base;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MacroRegionRepository extends JpaRepository<MacroRegion, Long>{
-    
-}
--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/domain/survey/HarvestRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.utfpr.cp.emater.midmipsystem.domain.survey;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HarvestRepository extends JpaRepository<Harvest, Long>{
-    
-}
