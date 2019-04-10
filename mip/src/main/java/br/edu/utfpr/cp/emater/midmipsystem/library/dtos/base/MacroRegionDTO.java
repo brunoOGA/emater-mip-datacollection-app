0608486c866cd6db79b9ed40d2f7478bd622dbb6
gabriel <gabriel@10.20.19.68>
2019-04-10 15:11:25 -0300

    - Created basic structure for MacroRegion;

--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/library/dtos/base/MacroRegionDTO.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/library/dtos/base/MacroRegionDTO.java
@@ -1,22 +1,20 @@
 package br.edu.utfpr.cp.emater.midmipsystem.library.dtos.base;
 
 import java.io.Serializable;
-import javax.annotation.ManagedBean;
-import javax.enterprise.context.RequestScoped;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
 @Builder
 public class MacroRegionDTO implements Serializable {
 
     private Long id;
-    private String name;
+    
+    @Size (min = 3, max = 50, message = "O nome da macrorregião deve ter entre 3 e 50 caracteres")
+    protected String name;
 }
--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/base/MacroRegionController.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/base/MacroRegionController.java
@@ -7,41 +7,41 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import javax.persistence.EntityExistsException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MacroRegionController implements ICRUDController<MacroRegionDTO> {
-                    
-    @Getter
-    @Setter
-    @Size (min = 5)
-    private String name;
-    
+public class MacroRegionController extends MacroRegionDTO implements ICRUDController<MacroRegionDTO> {
+
     private List<MacroRegionDTO> macroRegions;
-    
+
     public MacroRegionController() {
-        this.macroRegions = Stream.of(new MacroRegionDTO(new Long("1"), "Macro Noroeste"),new MacroRegionDTO(new Long("2"), "Macro Noroeste"),new MacroRegionDTO(new Long("3"), "Macro Noroeste"),new MacroRegionDTO(new Long("4"), "Macro Noroeste")).collect(Collectors.toList());
+        this.macroRegions = Stream.of(new MacroRegionDTO(new Long("1"), "Macro Noroeste"), new MacroRegionDTO(new Long("2"), "Macro Noroeste"), new MacroRegionDTO(new Long("3"), "Macro Noroeste"), new MacroRegionDTO(new Long("4"), "Macro Noroeste")).collect(Collectors.toList());
     }
-    
+
     @Override
     public List<MacroRegionDTO> readAll() {
         return macroRegions;
     }
-    
+
 //    public void delete (Long id) {
 ////        macroRegions.removeIf(c -> c.getId() == id);
 //        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "PrimeFaces Rocks."));
 //    }
-
     @Override
-    public String create () {
-        MacroRegionDTO dto = MacroRegionDTO.builder().id(new Long ("10")).name(this.name).build();
-        this.macroRegions.add(dto);
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Macrorregião criada com sucesso!"));
-        return "index.xhtml";
+    public String create() {
+        MacroRegionDTO dto = MacroRegionDTO.builder().id(new Long("10")).name(this.name).build();
+
+        if (this.readAll().stream().anyMatch(mr -> mr.getName().equalsIgnoreCase(dto.getName()))) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Info", "Já existe uma macrorregião com esse nome!"));
+            return "create.xhtml";
+            
+        } else {
+            this.macroRegions.add(dto);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Macrorregião criada com sucesso!"));
+            return "index.xhtml";
+        }
+        
     }
-    
+
 }
