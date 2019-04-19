6b05e587d591162ed132afba1c67e809c8a9a5b4
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2019-04-19 10:15:25 -0300

    - Imported entities and repository for Farmer, Person and Supervisor from master branch. - Amended imports;

---
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/entity/base/Supervisor.java
@@ -0,0 +1,31 @@
+package br.edu.utfpr.cp.emater.midmipsystem.entity.base;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Supervisor extends Person implements Serializable {
+
+    private String email;
+
+    @ManyToOne
+    private Region region;
+    
+    public Supervisor () {
+        super();
+    }
+
+    public Supervisor (Long id, String name, String email, Region region) {
+        this();
+
+        this.setId (id);
+        this.setName (name);
+        this.setEmail (email);
+        this.setRegion(region);
+    }
+    
+}
