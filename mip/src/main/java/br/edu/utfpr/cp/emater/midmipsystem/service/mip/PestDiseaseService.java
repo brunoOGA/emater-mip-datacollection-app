a18b6170f93f939e86725c181f87616a42ff6d74
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2019-05-01 11:37:26 -0300

    - Implemented mip.pestDisease.list and related code;

--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/service/mip/PestDiseaseService.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/service/mip/PestDiseaseService.java
@@ -1,10 +1,12 @@
 package br.edu.utfpr.cp.emater.midmipsystem.service.mip;
 
-import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.Pest;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.PestDisease;
 import br.edu.utfpr.cp.emater.midmipsystem.exception.AnyPersistenceException;
 import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityAlreadyExistsException;
 import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityInUseException;
 import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityNotFoundException;
+import br.edu.utfpr.cp.emater.midmipsystem.exception.SupervisorNotAllowedInCity;
+import br.edu.utfpr.cp.emater.midmipsystem.repository.mip.PestDiseaseRepository;
 import br.edu.utfpr.cp.emater.midmipsystem.repository.mip.PestRepository;
 import br.edu.utfpr.cp.emater.midmipsystem.service.ICRUDService;
 import java.util.ArrayList;
@@ -14,78 +16,95 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PestDiseaseService implements ICRUDService<Pest> {
+public class PestDiseaseService implements ICRUDService<PestDisease> {
 
-    private final PestRepository pestRepository;
+    private final PestDiseaseRepository pestDiseaseRepository;
 
     @Autowired
-    public PestDiseaseService(PestRepository aPestRepository) {
-        this.pestRepository = aPestRepository;
+    public PestDiseaseService(PestDiseaseRepository aPestDiseaseRepository) {
+        this.pestDiseaseRepository = aPestDiseaseRepository;
     }
 
     @Override
-    public List<Pest> readAll() {
-        return List.copyOf(pestRepository.findAll());
+    public List<PestDisease> readAll() {
+        return List.copyOf(pestDiseaseRepository.findAll());
     }
-    
-//    public List<Region> readAllRegions() {
-//        return this.regionService.readAll();
+
+
+//    @Override
+//    public Pest readById(Long anId) throws EntityNotFoundException {
+//        return pestRepository.findById(anId).orElseThrow(EntityNotFoundException::new);
+//    }
+//
+//    public void create(Pest aPest) throws EntityAlreadyExistsException, AnyPersistenceException {
+//
+//        if (pestRepository.findAll().stream().anyMatch(currentPest -> currentPest.equals(aPest))) {
+//            throw new EntityAlreadyExistsException();
+//        }
+//
+//        try {
+//            pestRepository.save(aPest);
+//
+//        } catch (Exception e) {
+//            throw new AnyPersistenceException();
+//        }
+//    }
+//
+//    public void update(Pest aPest) throws EntityAlreadyExistsException, EntityNotFoundException, AnyPersistenceException { 
+//        
+//        var existentPest = pestRepository.findById(aPest.getId()).orElseThrow(EntityNotFoundException::new);
+//        
+//        var allPestsWithoutExistentPest = new ArrayList<Pest>(pestRepository.findAll());
+//        allPestsWithoutExistentPest.remove(existentPest);
+//
+//        if (allPestsWithoutExistentPest.stream().anyMatch(currentPest -> currentPest.equals(aPest)))
+//            throw new EntityAlreadyExistsException();
+//                
+//        try {
+//            existentPest.setUsualName(aPest.getUsualName());
+//            existentPest.setScientificName(aPest.getScientificName());
+//            existentPest.setPestSize(aPest.getPestSize());
+//                        
+//            pestRepository.saveAndFlush(existentPest);
+//
+//        } catch (Exception e) {
+//            throw new AnyPersistenceException();
+//        }
+//    }
+//
+//    public void delete(Long anId) throws EntityNotFoundException, EntityInUseException, AnyPersistenceException {
+//        
+//        var existentPest = pestRepository.findById(anId).orElseThrow(EntityNotFoundException::new);
+//        
+//        try {
+//            pestRepository.delete(existentPest);
+//            
+//        } catch (DataIntegrityViolationException cve) {
+//            throw new EntityInUseException();
+//            
+//        } catch (Exception e) {
+//            throw new AnyPersistenceException();
+//        }
 //    }
 
     @Override
-    public Pest readById(Long anId) throws EntityNotFoundException {
-        return pestRepository.findById(anId).orElseThrow(EntityNotFoundException::new);
+    public void create(PestDisease entity) throws SupervisorNotAllowedInCity, EntityAlreadyExistsException, AnyPersistenceException, EntityNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void create(Pest aPest) throws EntityAlreadyExistsException, AnyPersistenceException {
-
-        if (pestRepository.findAll().stream().anyMatch(currentPest -> currentPest.equals(aPest))) {
-            throw new EntityAlreadyExistsException();
-        }
-
-        try {
-            pestRepository.save(aPest);
-
-        } catch (Exception e) {
-            throw new AnyPersistenceException();
-        }
+    @Override
+    public PestDisease readById(Long anId) throws EntityNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void update(Pest aPest) throws EntityAlreadyExistsException, EntityNotFoundException, AnyPersistenceException { 
-        
-        var existentPest = pestRepository.findById(aPest.getId()).orElseThrow(EntityNotFoundException::new);
-        
-        var allPestsWithoutExistentPest = new ArrayList<Pest>(pestRepository.findAll());
-        allPestsWithoutExistentPest.remove(existentPest);
-
-        if (allPestsWithoutExistentPest.stream().anyMatch(currentPest -> currentPest.equals(aPest)))
-            throw new EntityAlreadyExistsException();
-                
-        try {
-            existentPest.setUsualName(aPest.getUsualName());
-            existentPest.setScientificName(aPest.getScientificName());
-            existentPest.setPestSize(aPest.getPestSize());
-                        
-            pestRepository.saveAndFlush(existentPest);
-
-        } catch (Exception e) {
-            throw new AnyPersistenceException();
-        }
+    @Override
+    public void update(PestDisease entity) throws SupervisorNotAllowedInCity, EntityAlreadyExistsException, EntityNotFoundException, AnyPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    @Override
     public void delete(Long anId) throws EntityNotFoundException, EntityInUseException, AnyPersistenceException {
-        
-        var existentPest = pestRepository.findById(anId).orElseThrow(EntityNotFoundException::new);
-        
-        try {
-            pestRepository.delete(existentPest);
-            
-        } catch (DataIntegrityViolationException cve) {
-            throw new EntityInUseException();
-            
-        } catch (Exception e) {
-            throw new AnyPersistenceException();
-        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/mip/PestDiseaseController.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/mip/PestDiseaseController.java
@@ -1,12 +1,14 @@
 package br.edu.utfpr.cp.emater.midmipsystem.view.mip;
 
 import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.Pest;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.PestDisease;
 import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.PestSize;
 import br.edu.utfpr.cp.emater.midmipsystem.view.ICRUDController;
 import br.edu.utfpr.cp.emater.midmipsystem.exception.AnyPersistenceException;
 import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityAlreadyExistsException;
 import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityInUseException;
 import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityNotFoundException;
+import br.edu.utfpr.cp.emater.midmipsystem.service.mip.PestDiseaseService;
 import br.edu.utfpr.cp.emater.midmipsystem.service.mip.PestService;
 import java.util.Arrays;
 import java.util.List;
@@ -18,122 +20,148 @@ import org.springframework.web.context.annotation.RequestScope;
 
 @Component
 @RequestScope
-public class PestDiseaseController extends Pest implements ICRUDController<Pest> {
+public class PestDiseaseController extends PestDisease implements ICRUDController<PestDisease> {
 
-    private final PestService pestService;
+    private final PestDiseaseService pestDiseaseService;
     
     @Autowired
-    public PestDiseaseController(PestService aPestService) {
-        this.pestService = aPestService;
+    public PestDiseaseController(PestDiseaseService aPestDiseaseService) {
+        this.pestDiseaseService = aPestDiseaseService;
     }
 
     @Override
-    public List<Pest> readAll() {
-        return pestService.readAll();
+    public List<PestDisease> readAll() {
+        return pestDiseaseService.readAll();
     }
     
-    public List<PestSize> readAllPestSizes() {
-        return Arrays.asList(PestSize.values());
-    }    
+//    public List<PestSize> readAllPestSizes() {
+//        return Arrays.asList(PestSize.values());
+//    }    
+//
+//    @Override
+//    public String create() {
+//        var newPest = Pest.builder().usualName(this.getUsualName()).scientificName(this.getScientificName()).pestSize(this.getPestSize()).build();
+//
+//        try {
+//            pestService.create(newPest);
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", String.format("Inseto Praga [%s] criado com sucesso!", this.getUsualName())));
+//            return "index.xhtml";
+//
+//        } catch (EntityAlreadyExistsException e) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Já existe um inseto praga com esse nome e tamanho! Use um nome/tamanho diferente."));
+//            return "create.xhtml";
+//
+//        } catch (AnyPersistenceException e) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Erro na gravação dos dados!"));
+//            return "index.xhtml";
+//        }
+//    }
+//
+//    @Override
+//    public String prepareUpdate(Long anId) {
+//
+//        try {
+//            Pest existentPest = pestService.readById(anId);
+//            this.setId(existentPest.getId());
+//            this.setUsualName(existentPest.getUsualName());
+//            this.setScientificName(existentPest.getScientificName());
+//            this.setPestSize(existentPest.getPestSize());
+//            
+//            return "update.xhtml";
+//
+//        } catch (EntityNotFoundException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Inseto praga não pode ser alterado porque não foi encontrado na base de dados!"));
+//            return "index.xhtml";
+//        }
+//    }
+//
+//    @Override
+//    public String update() {
+//        var updatedPest = Pest.builder().id(this.getId()).usualName(this.getUsualName()).scientificName(this.getScientificName()).pestSize(this.getPestSize()).build();
+//
+//        try {
+//            pestService.update(updatedPest);
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Inseto praga alterado"));
+//            return "index.xhtml";
+//
+//        } catch (EntityAlreadyExistsException e) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Já existe um inseto praga com esse nome/tamanho! Use um nome/tamanho diferente."));
+//            return "update.xhtml";
+//
+//        } catch (EntityNotFoundException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Inseto praga não pode ser alterado porque não foi encontrado na base de dados!"));
+//            return "update.xhtml";
+//
+//        } catch (AnyPersistenceException e) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Erro na gravação dos dados!"));
+//            return "index.xhtml";
+//        }
+//
+//    }
+//
+//    @Override
+//    public String prepareDelete(Long anId) {
+//
+//        try {
+//            var existentPest = pestService.readById(anId);
+//            
+//            this.setId(existentPest.getId());
+//            this.setUsualName(existentPest.getUsualName());
+//            this.setPestSize(existentPest.getPestSize());
+//
+//            return "delete.xhtml";
+//
+//        } catch (EntityNotFoundException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Inseto praga não pode ser excluído porque não foi encontrado na base de dados!"));
+//            return "index.xhtml";
+//        }
+//    }
+//
+//    @Override
+//    public String delete() {
+//        
+//        try {
+//            pestService.delete(this.getId());
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Inseto praga excluído!"));
+//            return "index.xhtml";
+//
+//        } catch (EntityNotFoundException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Inseto praga não pode ser excluído porque não foi encontrado na base de dados!"));
+//            return "index.xhtml";
+//            
+//        } catch (EntityInUseException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Inseto praga não pode ser excluído porque está sendo usado em uma amostra!"));
+//            return "index.xhtml";
+//            
+//        } catch (AnyPersistenceException e) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Erro na gravação dos dados!"));
+//            return "index.xhtml";
+//        }
+//    }
 
     @Override
     public String create() {
-        var newPest = Pest.builder().usualName(this.getUsualName()).scientificName(this.getScientificName()).pestSize(this.getPestSize()).build();
-
-        try {
-            pestService.create(newPest);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", String.format("Inseto Praga [%s] criado com sucesso!", this.getUsualName())));
-            return "index.xhtml";
-
-        } catch (EntityAlreadyExistsException e) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Já existe um inseto praga com esse nome e tamanho! Use um nome/tamanho diferente."));
-            return "create.xhtml";
-
-        } catch (AnyPersistenceException e) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Erro na gravação dos dados!"));
-            return "index.xhtml";
-        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public String prepareUpdate(Long anId) {
-
-        try {
-            Pest existentPest = pestService.readById(anId);
-            this.setId(existentPest.getId());
-            this.setUsualName(existentPest.getUsualName());
-            this.setScientificName(existentPest.getScientificName());
-            this.setPestSize(existentPest.getPestSize());
-            
-            return "update.xhtml";
-
-        } catch (EntityNotFoundException ex) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Inseto praga não pode ser alterado porque não foi encontrado na base de dados!"));
-            return "index.xhtml";
-        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public String update() {
-        var updatedPest = Pest.builder().id(this.getId()).usualName(this.getUsualName()).scientificName(this.getScientificName()).pestSize(this.getPestSize()).build();
-
-        try {
-            pestService.update(updatedPest);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Inseto praga alterado"));
-            return "index.xhtml";
-
-        } catch (EntityAlreadyExistsException e) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Já existe um inseto praga com esse nome/tamanho! Use um nome/tamanho diferente."));
-            return "update.xhtml";
-
-        } catch (EntityNotFoundException ex) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Inseto praga não pode ser alterado porque não foi encontrado na base de dados!"));
-            return "update.xhtml";
-
-        } catch (AnyPersistenceException e) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Erro na gravação dos dados!"));
-            return "index.xhtml";
-        }
-
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public String prepareDelete(Long anId) {
-
-        try {
-            var existentPest = pestService.readById(anId);
-            
-            this.setId(existentPest.getId());
-            this.setUsualName(existentPest.getUsualName());
-            this.setPestSize(existentPest.getPestSize());
-
-            return "delete.xhtml";
-
-        } catch (EntityNotFoundException ex) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Inseto praga não pode ser excluído porque não foi encontrado na base de dados!"));
-            return "index.xhtml";
-        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public String delete() {
-        
-        try {
-            pestService.delete(this.getId());
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Inseto praga excluído!"));
-            return "index.xhtml";
-
-        } catch (EntityNotFoundException ex) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Inseto praga não pode ser excluído porque não foi encontrado na base de dados!"));
-            return "index.xhtml";
-            
-        } catch (EntityInUseException ex) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Inseto praga não pode ser excluído porque está sendo usado em uma amostra!"));
-            return "index.xhtml";
-            
-        } catch (AnyPersistenceException e) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Erro na gravação dos dados!"));
-            return "index.xhtml";
-        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
 }
