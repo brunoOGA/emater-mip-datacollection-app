7a54d7c82d7effd4c3bee2536bebba186e918d3f
gabriel <gabriel@10.20.19.68>
2019-05-08 14:38:17 -0300

    - Reimplemented MIPSample.list and related code;

---
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/service/mip/MIPSampleService.java
@@ -0,0 +1,182 @@
+package br.edu.utfpr.cp.emater.midmipsystem.service.mip;
+
+import br.edu.utfpr.cp.emater.midmipsystem.service.survey.*;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.base.Field;
+import br.edu.utfpr.cp.emater.midmipsystem.service.base.*;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.base.MacroRegion;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.MIPSample;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.survey.Harvest;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.survey.Survey;
+import br.edu.utfpr.cp.emater.midmipsystem.exception.AnyPersistenceException;
+import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityAlreadyExistsException;
+import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityInUseException;
+import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityNotFoundException;
+import br.edu.utfpr.cp.emater.midmipsystem.exception.SupervisorNotAllowedInCity;
+import br.edu.utfpr.cp.emater.midmipsystem.repository.mip.MIPSampleRepository;
+import br.edu.utfpr.cp.emater.midmipsystem.repository.survey.HarvestRepository;
+import br.edu.utfpr.cp.emater.midmipsystem.repository.survey.SurveyRepository;
+import br.edu.utfpr.cp.emater.midmipsystem.service.ICRUDService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MIPSampleService implements ICRUDService<MIPSample> {
+    
+    private final MIPSampleRepository mipSampleRepository;
+        
+    @Autowired
+    public MIPSampleService(MIPSampleRepository aMIPSampleRepository, SurveyService aSurveyService) {
+        this.mipSampleRepository = aMIPSampleRepository;
+    }
+
+    @Override
+    public List<MIPSample> readAll() {
+        return List.copyOf(mipSampleRepository.findAll());
+    }
+    
+    public List<Survey> readAllSurveysUniqueEntries() {
+        return List.copyOf(mipSampleRepository.findAll().stream().map(MIPSample::getSurvey).distinct().collect(Collectors.toList()));
+    }
+
+    @Override
+    public void create(MIPSample entity) throws SupervisorNotAllowedInCity, EntityAlreadyExistsException, AnyPersistenceException, EntityNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public MIPSample readById(Long anId) throws EntityNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(MIPSample entity) throws SupervisorNotAllowedInCity, EntityAlreadyExistsException, EntityNotFoundException, AnyPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Long anId) throws EntityNotFoundException, EntityInUseException, AnyPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+//    private final SurveyRepository surveyRepository;
+//    private final HarvestService harvestService;
+//    private final FieldService fieldService;
+//
+//    @Autowired
+//    public MIPSampleService(SurveyRepository aSurveyRepository, HarvestService aHarvestService, FieldService aFieldService) {
+//        this.surveyRepository = aSurveyRepository;
+//        this.harvestService = aHarvestService;
+//        this.fieldService = aFieldService;
+//    }
+//
+//    public List<Survey> readAll() {
+//        return List.copyOf(surveyRepository.findAll());
+//    }
+//
+//    public Harvest readHarvestById(Long id) throws EntityNotFoundException {
+//        return harvestService.readById(id);
+//    }
+//
+//    public Survey readById(Long anId) throws EntityNotFoundException {
+//        return surveyRepository.findById(anId).orElseThrow(EntityNotFoundException::new);
+//    }
+//
+//    public Field readFieldbyId(Long anId) throws EntityNotFoundException {
+//        return fieldService.readById(anId);
+//    }
+//    
+//    public List<Survey> readByHarvestId(Long harvestId) throws EntityNotFoundException {
+//        return List.copyOf(surveyRepository.findAll().stream().filter(currentSurvey -> currentSurvey.getHarvestId().equals(harvestId)).collect(Collectors.toList()));
+//    }
+//
+//    public void create(Survey aSurvey) throws SupervisorNotAllowedInCity, EntityAlreadyExistsException, AnyPersistenceException, EntityNotFoundException {
+//
+//        if (surveyRepository.findAll().stream().anyMatch(currentSurvey -> currentSurvey.equals(aSurvey))) {
+//            throw new EntityAlreadyExistsException();
+//        }
+//
+//        var theField = fieldService.readById(aSurvey.getFieldId());
+//        var theHarvest = harvestService.readById(aSurvey.getHarvestId());
+//
+//        aSurvey.setField(theField);
+//        aSurvey.setHarvest(theHarvest);
+//
+//        try {
+//            surveyRepository.save(aSurvey);
+//
+//        } catch (Exception e) {
+//            throw new AnyPersistenceException();
+//        }
+//    }
+//
+//    public void update(Survey aSurvey) throws EntityAlreadyExistsException, EntityNotFoundException, AnyPersistenceException {
+//
+//        var existentSurvey = surveyRepository.findById(aSurvey.getId()).orElseThrow(EntityNotFoundException::new);
+//
+//        try {
+//            existentSurvey.getQuestionData().setBt(aSurvey.isBt());
+//            existentSurvey.getQuestionData().setRustResistant(aSurvey.isRustResistant());
+//            
+//            existentSurvey.getDateData().setEmergenceDate(aSurvey.getEmergenceDate());
+//            existentSurvey.getDateData().setHarvestDate(aSurvey.getHarvestDate());
+//            existentSurvey.getDateData().setSowedDate(aSurvey.getSowedDate());
+//            
+//            existentSurvey.getSizeData().setPlantPerMeter(aSurvey.getPlantPerMeter());
+//            existentSurvey.getSizeData().setTotalArea(aSurvey.getTotalArea());
+//            existentSurvey.getSizeData().setTotalPlantedArea(aSurvey.getTotalPlantedArea());
+//            
+//            existentSurvey.getLocationData().setLatitude(aSurvey.getLatitude());
+//            existentSurvey.getLocationData().setLongitude(aSurvey.getLongitude());
+//            
+//            existentSurvey.getProductivityData().setProductivityFarmer(aSurvey.getProductivityFarmer());
+//            existentSurvey.getProductivityData().setProductivityField(aSurvey.getProductivityField());
+//            existentSurvey.getProductivityData().setSeparatedWeight(aSurvey.isSeparatedWeight());
+//            
+//            existentSurvey.setSeedName(aSurvey.getSeedName());
+//            existentSurvey.setSporeCollectorPresent(aSurvey.isSporeCollectorPresent());
+//            
+//            surveyRepository.saveAndFlush(existentSurvey);
+//
+//        } catch (Exception e) {
+//            throw new AnyPersistenceException();
+//        }
+//    }
+//
+//    public void delete(Long anId) throws EntityNotFoundException, EntityInUseException, AnyPersistenceException {
+//        
+//        var existentHarvest = surveyRepository.findById(anId).orElseThrow(EntityNotFoundException::new);
+//        
+//        try {
+//            surveyRepository.delete(existentHarvest);
+//            
+//        } catch (DataIntegrityViolationException cve) {
+//            throw new EntityInUseException();
+//            
+//        } catch (Exception e) {
+//            throw new AnyPersistenceException();
+//        }
+//    }
+//
+//    public List<Harvest> readAllHarvests() {
+//        return harvestService.readAll();
+//    }
+//
+//    public List<Field> readAllFieldsOutOfCurrentHarvest(Long harvestId) {
+//        var fieldsInCurrentHarvest = this.surveyRepository.findAll().stream()
+//                .filter(currentSurvey -> currentSurvey.getHarvestId().equals(harvestId))
+//                .map(Survey::getField)
+//                .collect(Collectors.toList());
+//
+//        var allFields = this.fieldService.readAll();
+//
+//        var allFieldsOutOfCurrentHarvest = new ArrayList<Field>(allFields);
+//
+//        allFieldsOutOfCurrentHarvest.removeAll(fieldsInCurrentHarvest);
+//
+//        return allFieldsOutOfCurrentHarvest;
+//    }
+}
---
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/mip/MIPSampleController.java
@@ -0,0 +1,344 @@
+package br.edu.utfpr.cp.emater.midmipsystem.view.mip;
+
+import br.edu.utfpr.cp.emater.midmipsystem.view.survey.*;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.base.Field;
+import br.edu.utfpr.cp.emater.midmipsystem.view.base.*;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.base.MacroRegion;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.MIPSample;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.survey.Harvest;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.survey.Survey;
+import br.edu.utfpr.cp.emater.midmipsystem.service.base.MacroRegionService;
+import br.edu.utfpr.cp.emater.midmipsystem.view.ICRUDController;
+import br.edu.utfpr.cp.emater.midmipsystem.exception.AnyPersistenceException;
+import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityAlreadyExistsException;
+import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityInUseException;
+import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityNotFoundException;
+import br.edu.utfpr.cp.emater.midmipsystem.exception.SupervisorNotAllowedInCity;
+import br.edu.utfpr.cp.emater.midmipsystem.service.mip.MIPSampleService;
+import br.edu.utfpr.cp.emater.midmipsystem.service.survey.HarvestService;
+import br.edu.utfpr.cp.emater.midmipsystem.service.survey.SurveyService;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component (value = "mipSampleController")
+@RequestScope
+public class MIPSampleController extends MIPSample implements ICRUDController<MIPSample> {
+    
+    private final MIPSampleService mipSampleService;
+    
+    @Autowired
+    public MIPSampleController(MIPSampleService aMIPSampleService) {
+        this.mipSampleService = aMIPSampleService;
+        
+    }
+
+    @Override
+    public List<MIPSample> readAll() {
+        return mipSampleService.readAll();
+    }
+    
+    public List<Survey> readAllSurveysUniqueEntries() {
+        return mipSampleService.readAllSurveysUniqueEntries();
+    }
+
+    @Override
+    public String create() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String prepareUpdate(Long anId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String prepareDelete(Long anId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String delete() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+//    private final SurveyService surveyService;
+//
+//    @Getter
+//    @Setter
+//    private Long selectedHarvestId;
+//    
+//    @Getter
+//    @Setter
+//    private Long selectedFieldId;
+//
+//    @Getter
+//    @Setter
+//    private boolean rustResistant;
+//
+//    @Getter
+//    @Setter
+//    private boolean bt;
+//
+//    @Getter
+//    @Setter
+//    private double totalArea;
+//
+//    @Getter
+//    @Setter
+//    private double totalPlantedArea;
+//
+//    @Getter
+//    @Setter
+//    private double plantPerMeter;
+//
+//    @Getter
+//    @Setter
+//    private double longitude;
+//
+//    @Getter
+//    @Setter
+//    private double latitude;
+//
+//    @Getter
+//    @Setter
+//    private double productivityField;
+//
+//    @Getter
+//    @Setter
+//    private double productivityFarmer;
+//
+//    @Getter
+//    @Setter
+//    private boolean separatedWeight;
+//
+//    @Getter
+//    @Setter
+//    private Date sowedDate;
+//
+//    @Getter
+//    @Setter
+//    private Date emergenceDate;
+//
+//    @Getter
+//    @Setter
+//    private Date harvestDate;
+//
+//    @Autowired
+//    public MIPSampleController(SurveyService aSurveyService) {
+//        this.surveyService = aSurveyService;
+//    }
+//
+//    @Override
+//    public List<Survey> readAll() {
+//        return surveyService.readAll();
+//    }
+//
+//    public List<Harvest> readAllHarvests() {
+//        return surveyService.readAllHarvests();
+//    }
+//
+//    public List<Field> readAllFieldsOutOfCurrentSurvey() {
+//        return surveyService.readAllFieldsOutOfCurrentHarvest(this.getSelectedHarvestId());
+//    }
+//
+//    public String selectHarvest() {
+//
+//        try {
+//            var selectedHarvest = surveyService.readHarvestById(this.getSelectedHarvestId());
+//
+//            this.setHarvest(selectedHarvest);
+//            this.setSelectedHarvestId(selectedHarvest.getId());
+//
+//            return "create.xhtml";
+//
+//        } catch (EntityNotFoundException e) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Safra não pode ser selecionada porque não foi encontrada na base de dados!"));
+//            return "index.xhtml";
+//        }
+//
+//    }
+//
+//    @Override
+//    public String create() {
+//
+//        try {
+//            var newSurvey = Survey.builder()
+//                    .bt(this.isBt())
+//                    .emergenceDate(this.getEmergenceDate())
+//                    .field(this.getField())
+//                    .harvest(surveyService.readHarvestById(this.getSelectedHarvestId()))
+//                    .latitude(this.getLatitude())
+//                    .longitude(this.getLongitude())
+//                    .plantPerMeter(this.getPlantPerMeter())
+//                    .productivityFarmer(this.getProductivityFarmer())
+//                    .productivityField(this.getProductivityField())
+//                    .rustResistant(this.isRustResistant())
+//                    .seedName(this.getSeedName())
+//                    .separatedWeight(this.isSeparatedWeight())
+//                    .sowedDate(this.getSowedDate())
+//                    .harvestDate(this.getHarvestDate())
+//                    .sporeCollectorPresent(this.isSporeCollectorPresent())
+//                    .totalArea(this.getTotalArea())
+//                    .totalPlantedArea(this.totalPlantedArea)
+//                    .build();
+//
+//            surveyService.create(newSurvey);
+//
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", String.format("UR [%s] adicionada na pesquisa da [%s]", newSurvey.getFieldName(), newSurvey.getHarvestName())));
+//
+//            return "index.xhtml";
+//
+//        } catch (EntityAlreadyExistsException e) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "A unidade de referência já faz parte dessa pesquisa."));
+//            return "create.xhtml";
+//
+//        } catch (EntityNotFoundException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Não foi possível adicionar a UR à pesquisa porque a safra ou a UR não foram encontradas na base de dados!"));
+//            return "index.xhtml";
+//
+//        } catch (AnyPersistenceException | SupervisorNotAllowedInCity ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Erro na gravação dos dados!"));
+//            return "index.xhtml";
+//
+//        }
+//    }
+//
+//    @Override
+//    public String prepareUpdate(Long anId) {
+//
+//        try {
+//            var existentSurvey = surveyService.readById(anId);
+//
+//            this.setId(existentSurvey.getId());
+//            this.setSeedName(existentSurvey.getSeedName());
+//            this.setSporeCollectorPresent(existentSurvey.isSporeCollectorPresent());
+//            
+//            this.setField(existentSurvey.getField());
+//            this.setSelectedFieldId(existentSurvey.getFieldId());
+//            
+//            this.setHarvest(existentSurvey.getHarvest());
+//            this.setSelectedHarvestId(existentSurvey.getHarvestId());
+//
+//            this.setBt(existentSurvey.isBt());
+//            this.setEmergenceDate(existentSurvey.getEmergenceDate());
+//            this.setHarvestDate(existentSurvey.getHarvestDate());
+//            this.setLatitude(existentSurvey.getLatitude());
+//            this.setLongitude(existentSurvey.getLongitude());
+//            this.setPlantPerMeter(existentSurvey.getPlantPerMeter());
+//            this.setProductivityFarmer(existentSurvey.getProductivityFarmer());
+//            this.setProductivityField(existentSurvey.getProductivityField());
+//            this.setRustResistant(existentSurvey.isRustResistant());
+//            this.setSeparatedWeight(existentSurvey.isSeparatedWeight());
+//            this.setSowedDate(existentSurvey.getSowedDate());
+//            this.setTotalArea(existentSurvey.getTotalArea());
+//            this.setTotalPlantedArea(existentSurvey.getTotalPlantedArea());
+//            
+//            return "update.xhtml";
+//
+//        } catch (EntityNotFoundException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Dados não puderam ser alterados porque não foram encontrados na base de dados!"));
+//            return "index.xhtml";
+//        }
+//    }
+//
+//    @Override
+//    public String update() {
+//
+//        try {
+//
+//            var updatedSurvey = Survey.builder()
+//                    .id(this.getId())
+//                    .bt(this.isBt())
+//                    .emergenceDate(this.getEmergenceDate())
+//                    .field(surveyService.readFieldbyId(this.getSelectedFieldId()))
+//                    .harvest(surveyService.readHarvestById(this.getSelectedHarvestId()))
+//                    .latitude(this.getLatitude())
+//                    .longitude(this.getLongitude())
+//                    .plantPerMeter(this.getPlantPerMeter())
+//                    .productivityFarmer(this.getProductivityFarmer())
+//                    .productivityField(this.getProductivityField())
+//                    .rustResistant(this.isRustResistant())
+//                    .seedName(this.getSeedName())
+//                    .separatedWeight(this.isSeparatedWeight())
+//                    .sowedDate(this.getSowedDate())
+//                    .harvestDate(this.getHarvestDate())
+//                    .sporeCollectorPresent(this.isSporeCollectorPresent())
+//                    .totalArea(this.getTotalArea())
+//                    .totalPlantedArea(this.totalPlantedArea)
+//                    .build();
+//
+//            surveyService.update(updatedSurvey);
+//            
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "UR alterada!"));
+//            return "index.xhtml";
+//
+//        } catch (EntityAlreadyExistsException e) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "A unidade de referência já faz parte dessa pesquisa."));
+//            return "update.xhtml";
+//
+//        } catch (EntityNotFoundException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro",  "Não foi possível adicionar a UR à pesquisa porque a safra ou a UR não foram encontradas na base de dados!"));
+//            return "update.xhtml";
+//
+//        } catch (AnyPersistenceException e) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Erro na gravação dos dados!"));
+//            return "index.xhtml";
+//        }
+//
+//    }
+//
+//    @Override
+//    public String prepareDelete(Long anId) {
+//
+//        try {
+//            var existentSurvey = surveyService.readById(anId);
+//            
+//            this.setId(existentSurvey.getId());
+//            this.setField(existentSurvey.getField());
+//            this.setHarvest(existentSurvey.getHarvest());
+//
+//            return "delete.xhtml";
+//
+//        } catch (EntityNotFoundException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "UR não pode ser removida da pesquisa porque não foi encontrada na base de dados!"));
+//            return "index.xhtml";
+//        }
+//    }
+//
+//    @Override
+//    public String delete() {
+//        
+//        try {
+//            surveyService.delete(this.getId());
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "UR removida da pesquisa!"));
+//            return "index.xhtml";
+//
+//        } catch (EntityNotFoundException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "UR não pode ser removida da pesquisa porque não foi encontrada na base de dados!"));
+//            return "index.xhtml";
+//            
+//        } catch (EntityInUseException ex) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "UR não pode ser removida da pesquisa porque já existem dados MID/MIP para para!"));
+//            return "index.xhtml";
+//            
+//        } catch (AnyPersistenceException e) {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Erro na gravação dos dados!"));
+//            return "index.xhtml";
+//        }
+//    }
+
+}
