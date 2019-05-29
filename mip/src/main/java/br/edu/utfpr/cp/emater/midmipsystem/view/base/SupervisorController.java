6ba8977dd319beb5d8bfde1186d4e28cf5022c49
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2019-05-29 14:35:30 -0300

    - Replaced all Bootsfaces components with Primefaces; - Supervisor updated!

--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/base/SupervisorController.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/base/SupervisorController.java
@@ -15,6 +15,8 @@ import br.edu.utfpr.cp.emater.midmipsystem.service.base.SupervisorService;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
@@ -24,6 +26,10 @@ import org.springframework.web.context.annotation.RequestScope;
 public class SupervisorController extends Supervisor implements ICRUDController<Supervisor> {
 
     private SupervisorService supervisorService;
+    
+    @Getter
+    @Setter
+    private Long selectedRegionId;
     
     @Autowired
     public SupervisorController(SupervisorService aSupervisorService) {
@@ -41,7 +47,7 @@ public class SupervisorController extends Supervisor implements ICRUDController<
 
     @Override
     public String create() {
-        var newSupervisor = Supervisor.builder().name(this.getName()).email(this.getEmail()).region(this.getRegion()).build();
+        var newSupervisor = Supervisor.builder().name(this.getName()).email(this.getEmail()).region(this.supervisorService.readRegionById(this.getSelectedRegionId())).build();
 
         try {
             supervisorService.create(newSupervisor);
@@ -66,7 +72,8 @@ public class SupervisorController extends Supervisor implements ICRUDController<
             this.setId(existentSupervisor.getId());
             this.setName(existentSupervisor.getName());
             this.setEmail(existentSupervisor.getEmail());
-            this.setRegion(existentSupervisor.getRegion());
+            this.setSelectedRegionId(existentSupervisor.getRegionId());
+//            this.setRegion(existentSupervisor.getRegion());
 
             return "update.xhtml";
 
@@ -78,7 +85,7 @@ public class SupervisorController extends Supervisor implements ICRUDController<
 
     @Override
     public String update() {
-        var updatedSupervisor = Supervisor.builder().id(this.getId()).name(this.getName()).email(this.getEmail()).region(this.getRegion()).build();
+        var updatedSupervisor = Supervisor.builder().id(this.getId()).name(this.getName()).email(this.getEmail()).region(this.supervisorService.readRegionById(this.getSelectedRegionId())).build();
 
         try {
             supervisorService.update(updatedSupervisor);
