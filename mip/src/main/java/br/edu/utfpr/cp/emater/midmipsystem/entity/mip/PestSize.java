2d4bc7ef5527a2c63fe3ad3a3c09dc63f9cde0b4
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2019-09-13 14:23:41 -0300

    - Fixed MIP.PestSize options;

--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/entity/mip/PestSize.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/entity/mip/PestSize.java
@@ -2,9 +2,10 @@ package br.edu.utfpr.cp.emater.midmipsystem.entity.mip;
 
 public enum PestSize {
     
-    MAIOR_15CM ("> 15 cm"),
-    MENOR_15CM ("< 15 cm"),
-    TERCEIRO_AO_QUINTO_INSTAR ("3. ao 5. Instar"),
+    MAIOR_15CM (">= 1,5 cm"),
+    MENOR_15CM ("< 1,5 cm"),
+    TERCEIRO_AO_QUINTO_INSTAR ("Ninfa (3. ao 5. instar)"),
+    PONTEIROS_ATACADOS ("Ponteiros atacados"),
     ADULTOS ("Adultos");
     
     private String name;
