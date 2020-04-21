5ef287e964b97cc014a421b7b573c22f6adf415b
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2020-04-21 16:25:41 -0300

    - Removed unnecessary authentication provider configuration; - This is part of issue #203;

--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/service/security/Configuration.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/service/security/Configuration.java
@@ -3,11 +3,8 @@ package br.edu.utfpr.cp.emater.midmipsystem.service.security;
 import java.io.Serializable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -15,28 +12,6 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class Configuration extends WebSecurityConfigurerAdapter implements Serializable {
-
-    private final MIPUserDetailsService mIPUserDetailsService;
-
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        return mIPUserDetailsService;
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
