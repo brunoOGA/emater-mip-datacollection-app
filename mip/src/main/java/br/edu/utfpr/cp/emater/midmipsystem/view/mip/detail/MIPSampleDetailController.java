b674a4cf1f72718c1b79f3a475ad9f55764d62f5
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2020-05-01 15:32:27 -0300

    - Removed all reference to editable table from View.MIPSample; - OBS: it was becoming to much complex to enable editing on the table; - This is part of issue #200.

--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/mip/detail/MIPSampleDetailController.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/mip/detail/MIPSampleDetailController.java
@@ -1,9 +1,6 @@
 package br.edu.utfpr.cp.emater.midmipsystem.view.mip.detail;
 
 import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.MIPSample;
-import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.MIPSampleNaturalPredatorOccurrence;
-import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.MIPSamplePestDiseaseOccurrence;
-import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.MIPSamplePestOccurrence;
 import br.edu.utfpr.cp.emater.midmipsystem.entity.survey.Survey;
 import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityNotFoundException;
 import br.edu.utfpr.cp.emater.midmipsystem.service.mip.MIPSampleService;
@@ -15,7 +12,6 @@ import javax.faces.context.FacesContext;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.primefaces.event.RowEditEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
@@ -29,68 +25,6 @@ public class MIPSampleDetailController {
     @Setter
     @Getter
     private Survey currentSurvey;
-
-    @Setter
-    @Getter
-    private double pestValue;
-
-    @Setter
-    @Getter
-    private double pestDiseaseValue;
-
-    @Setter
-    @Getter
-    private double pestNaturalPredatorValue;
-
-    private void doUpdate(MIPSample aSample) {
-
-        try {
-            mipSampleService.update(aSample);
-
-            this.setPestValue(0.0);
-            this.setPestDiseaseValue(0.0);
-            this.setPestNaturalPredatorValue(0.0);
-
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Edição feita!", "Edição feita"));
-
-        } catch (Exception ex) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Edição nao salva!", "Edição nao salva"));
-        }
-    }
-
-    public void onPestOccurrenceEdit(RowEditEvent event) {
-        var currentMIPSample = (MIPSample) event.getComponent().getAttributes().get("currentMIPSample");
-
-        var pestOccurrence = (MIPSamplePestOccurrence) event.getObject();
-
-        pestOccurrence.setValue(this.getPestValue());
-
-        this.doUpdate(currentMIPSample);
-    }
-
-    public void onPestDiseaseOccurrenceEdit(RowEditEvent event) {
-        var currentMIPSample = (MIPSample) event.getComponent().getAttributes().get("currentMIPSample");
-
-        var pestDiseaseOccurrence = (MIPSamplePestDiseaseOccurrence) event.getObject();
-
-        pestDiseaseOccurrence.setValue(this.getPestDiseaseValue());
-
-        this.doUpdate(currentMIPSample);
-    }
-
-    public void onPestNaturalPredatorOccurrenceEdit(RowEditEvent event) {
-        var currentMIPSample = (MIPSample) event.getComponent().getAttributes().get("currentMIPSample");
-
-        var pestNaturalPredatorOccurrence = (MIPSampleNaturalPredatorOccurrence) event.getObject();
-
-        pestNaturalPredatorOccurrence.setValue(this.getPestNaturalPredatorValue());
-
-        this.doUpdate(currentMIPSample);
-    }
-
-    public void onRowCancel(RowEditEvent event) {
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Edição cancelada", "Cancelada!"));
-    }
 
     public List<MIPSample> readAllMIPSampleBySurvey() {
         return mipSampleService.readAllMIPSampleBySurveyId(this.getCurrentSurvey().getId());
