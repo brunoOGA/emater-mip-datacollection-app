a57c45735267ee3b09130d187a6fb5c0404e6ce9
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2020-05-01 14:29:52 -0300

    - Added cell editor for pestDisease and pestNaturalPredator - This is part of issue #200.

--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/mip/detail/MIPSampleDetailController.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/mip/detail/MIPSampleDetailController.java
@@ -1,6 +1,8 @@
 package br.edu.utfpr.cp.emater.midmipsystem.view.mip.detail;
 
 import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.MIPSample;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.MIPSampleNaturalPredatorOccurrence;
+import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.MIPSamplePestDiseaseOccurrence;
 import br.edu.utfpr.cp.emater.midmipsystem.entity.mip.MIPSamplePestOccurrence;
 import br.edu.utfpr.cp.emater.midmipsystem.entity.survey.Survey;
 import br.edu.utfpr.cp.emater.midmipsystem.exception.EntityNotFoundException;
@@ -30,29 +32,59 @@ public class MIPSampleDetailController {
     @Setter
     @Getter
     private Survey currentSurvey;
-    
+
     @Setter
     @Getter
     private double value;
-    
-    public void onRowEdit(RowEditEvent event) {
+
+    public void onPestOccurrenceEdit(RowEditEvent event) {
         var currentMIPSample = (MIPSample) event.getComponent().getAttributes().get("currentMIPSample");
-        
+
         var pestOccurrence = (MIPSamplePestOccurrence) event.getObject();
 
         pestOccurrence.setValue(this.getValue());
-        
+
         try {
             mipSampleService.update(currentMIPSample);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Edição feita!", ""+ pestOccurrence.getValue()));
-            
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Edição feita!", "" + pestOccurrence.getValue()));
+
         } catch (Exception ex) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Edição nao salva!", ""+ pestOccurrence.getValue()));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Edição nao salva!", "" + pestOccurrence.getValue()));
         }
-        
-        
     }
-     
+
+    public void onPestDiseaseOccurrenceEdit(RowEditEvent event) {
+        var currentMIPSample = (MIPSample) event.getComponent().getAttributes().get("currentMIPSample");
+
+        var pestDiseaseOccurrence = (MIPSamplePestDiseaseOccurrence) event.getObject();
+
+        pestDiseaseOccurrence.setValue(this.getValue());
+
+        try {
+            mipSampleService.update(currentMIPSample);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Edição feita!", "" + pestDiseaseOccurrence.getValue()));
+
+        } catch (Exception ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Edição nao salva!", "" + pestDiseaseOccurrence.getValue()));
+        }
+    }
+
+    public void onPestNaturalPredatorOccurrenceEdit(RowEditEvent event) {
+        var currentMIPSample = (MIPSample) event.getComponent().getAttributes().get("currentMIPSample");
+
+        var pestNaturalPredatorOccurrence = (MIPSampleNaturalPredatorOccurrence) event.getObject();
+
+        pestNaturalPredatorOccurrence.setValue(this.getValue());
+
+        try {
+            mipSampleService.update(currentMIPSample);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Edição feita!", "" + pestNaturalPredatorOccurrence.getValue()));
+
+        } catch (Exception ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Edição nao salva!", "" + pestNaturalPredatorOccurrence.getValue()));
+        }
+    }
+
     public void onRowCancel(RowEditEvent event) {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Edição cancelada", "Cancelada!"));
     }
@@ -80,18 +112,20 @@ public class MIPSampleDetailController {
 
     @Deprecated
     public int calculateDaysAfterEmergence(Date sampleDate) {
-        
-        if (currentSurvey.getEmergenceDate() == null)
+
+        if (currentSurvey.getEmergenceDate() == null) {
             return 0;
-        
+        }
+
         long diffInMillies = (sampleDate.getTime() - currentSurvey.getEmergenceDate().getTime());
 
         if (diffInMillies > 0) {
             var result = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
 
             return (int) (result + 1);
-            
-        } else
+
+        } else {
             return 0;
+        }
     }
 }
